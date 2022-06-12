32027ce6ce8fc10bd014258b8dfcbac38e31d7d5
Amir Modan <amir5modan@gmail.com>
2022-06-12 16:46:48 -0700

    Removed Extra Imports from BluetoothConnect.java

--- app/src/main/java/TenaSensor/Android/BluetoothConnect.java
+++ app/src/main/java/TenaSensor/Android/BluetoothConnect.java
@@ -1,13 +1,10 @@
 package TenaSensor.Android;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,16 +13,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Set;
-import java.util.UUID;
 
 /**
  * @author Amir Modan (amir5modan@gmail.com)
