46bb5894a5488548041a63e5e96de0e7290dbf6c
Amir Modan <amir5modan@gmail.com>
2022-06-12 19:44:13 -0700

    Added Documentation to ExerciseInstructions.java

--- app/src/main/java/TenaSensor/Android/ExerciseInstructions.java
+++ app/src/main/java/TenaSensor/Android/ExerciseInstructions.java
@@ -10,36 +10,57 @@ import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+/**
+ * @author Amir Modan (amir5modan@gmail.com)
+ * Activity in which instructions are given for the exercise to be performed
+ *
+ * Functions include:
+ *  Displaying instructions for the selected exercise in text
+ *  Playing a video of the exercise being performed
+ */
 public class ExerciseInstructions extends AppCompatActivity {
 
+    // Declare GUI components
     private Button startBtn, homeBtn;
-    public static String exercise = "Exercise";
+
+    // Key for accessing information from previous class, ExerciseSelection.java
+    private String exercise = "Exercise";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
 
+        // Assign models to views
         VideoView videoView = (VideoView) findViewById(R.id.videoView);  //casting to VideoView is not Strictly required above API level 26
         TextView exerciseName = findViewById(R.id.ExerciseText);
         TextView exerciseInstructions = findViewById(R.id.ExerciseInstructions);
+
+        // Get selected exercise from previous class, ExerciseSelection.java
         Bundle bundle = getIntent().getExtras();
         switch (bundle.getString(exercise)) {
+            // When Block Placing was selected, display instructions for Block Placing
             case "BlockPlacing":
                 exerciseName.setText("Block Placing");
                 exerciseInstructions.setText(R.string.block_instructions);
                 videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.block_video); //set the path of the video that we need to use in our VideoView
                 break;
+
+            // When Finger-to-Nose was selected, display instructions for Finger-to-Nose
             case "FingerToNose":
                 exerciseName.setText("Finger To Nose");
                 exerciseInstructions.setText(R.string.water_instructions);
                 videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.block_video); //set the path of the video that we need to use in our VideoView
                 break;
+
+            // When Cup Drinking was selected, display instructions for Cup Drinking
             case "CupDrinking":
                 exerciseName.setText("Cup Drinking");
                 exerciseInstructions.setText(R.string.cup_instructions);
                 videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.block_video); //set the path of the video that we need to use in our VideoView
                 break;
+
+            // When Rod Placing was selected, display instructions for Rod Placing
             case "RodPlacing":
                 exerciseName.setText("Rod Placing");
                 exerciseInstructions.setText(R.string.rod_instructions);
@@ -47,32 +68,39 @@ public class ExerciseInstructions extends AppCompatActivity {
                 break;
         }
 
+        // Start Playing Video
         videoView.start();
 
+        // Grants user access to video controls
         MediaController mediaController = new MediaController(this);
-        //link mediaController to videoView
+        // Link mediaController to videoView
         mediaController.setAnchorView(videoView);
-        //allow mediaController to control our videoView
+        // Allow mediaController to control our videoView
         videoView.setMediaController(mediaController);
 
+        // Assign button models to views
         startBtn = (Button)findViewById(R.id.StartExerciseButton);
+        homeBtn = (Button)findViewById(R.id.home_button);
 
+        // Set Action Bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.header);
 
-        homeBtn = (Button)findViewById(R.id.home_button);
-
+        // When Home Button is clicked
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Goes to Home Page
                 Intent intent = new Intent(ExerciseInstructions.this, MainActivity.class);
                 ExerciseInstructions.this.startActivity(intent);
             }
         });
 
+        // When Start Button is clicked
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Goes to Exercise
                 Intent intent = new Intent(ExerciseInstructions.this, ExercisePerform.class);
                 ExerciseInstructions.this.startActivity(intent);
             }
